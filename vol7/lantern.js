7747c84ad11f9519c89de6fdf27bc6ba895b9931
kree <kree@kree.roam.corp.google.com>
2017-02-17 19:01:28 -0800

    wazzup peeps

--- vol7/lantern.js
+++ vol7/lantern.js
@@ -1,11 +1,30 @@
 var five = require('johnny-five');
-var board = new five.board();
+var board = new five.Board();
 
 board.on('ready', function() {
-var green = new five.Led(11);
+var green = new five.Led(9);
 var red = new five.Led(10);
-var blue = new five.Led(9);
-red.pulse(2000);
-blue.pulse(1500);
-green.pulse(1000);
+var blue = new five.Led(11);
+
+red.pulse({
+  easing: "linear",
+  duration: 7500,
+  cuePoints: [0, 0.27, 0.33, 0.6, 0.67, 0.93, 1],
+  keyFrames: [255, 127, 0, 0, 0, 0, 255],
+  metronomic: false,
+});
+blue.pulse({
+  easing: "linear",
+  duration: 7500,
+  cuePoints: [0, 0.27, 0.33, 0.6, 0.67, 0.93, 1],
+  keyFrames: [0, 0, 255, 127, 0, 0, 0],
+  metronomic: false,
+});
+green.pulse({
+  easing: "linear",
+  duration: 7500,
+  cuePoints: [0, 0.27, 0.33, 0.6, 0.67, 0.93, 1],
+  keyFrames: [0, 0, 0, 0, 255, 127, 0],
+  metronomic: false,
+});
 });
